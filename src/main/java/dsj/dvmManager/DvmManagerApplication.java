dbd306c8945e9394506e62e5c33a2d4a3184d23f
GOETTTO <tgoettel9401@yahoo.de>
2020-11-29 23:24:52 +0100

    Start Browser automatically during startup

--- src/main/java/dsj/dvmManager/DvmManagerApplication.java
+++ src/main/java/dsj/dvmManager/DvmManagerApplication.java
@@ -3,11 +3,27 @@ package dsj.dvmManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class DvmManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DvmManagerApplication.class, args);
+		openHomePage();
+	}
+
+	private static void openHomePage() {
+		Runtime rt = Runtime.getRuntime();
+		try {
+			rt.exec("cmd /c start chrome.exe http://localhost:8081");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
